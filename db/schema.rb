08b5ebc2fde21ea991b23bbfb1d0816ae1cd40a0
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-08-12 20:54:46 -0700

    Add unique index on user_projects table
    Each user should only be able to have one user_project record per project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_175121) do
+ActiveRecord::Schema.define(version: 2020_08_13_035246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_175121) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id", "project_id"], name: "index_user_projects_on_user_id_and_project_id", unique: true
     t.index ["user_id"], name: "index_user_projects_on_user_id"
   end
 
